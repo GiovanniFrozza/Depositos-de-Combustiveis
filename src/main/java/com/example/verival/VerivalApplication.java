c070ecd8f9ed75504158e8847bc020685c438adf
giovannifrozza <giovannifrozza470@gmail.com>
2021-09-27 21:23:52 -0300

    first commit

---
+++ src/main/java/com/example/verival/VerivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.verival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/verival/VerivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
