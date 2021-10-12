1e29adc3457068a1f8801779a6c65be94d05f185
giovannifrozza <giovannifrozza470@gmail.com>
2021-10-12 16:28:28 -0300

    testes

--- src/test/java/com/example/verival/AbasteceValorLimiteTeste.java
+++ src/test/java/com/example/verival/AbasteceValorLimiteTeste.java
@@ -3,12 +3,11 @@ package com.example.verival;
 import org.junit.Assert;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AbasteceTeste {
+public class AbasteceValorLimiteTeste {
+
     @ParameterizedTest
-    @CsvSource({ "0, 0, -1", "200, 0, -1", "500, 0, -1", "0, -10, -1", "200,-10, -1", "500, -10, -1", "0, 350, 350", "200, 350, 300", "500, 350, 0" })
+    @CsvSource({ "150, 500, 350", "380, 500, 120", "492, 500, 8", "150, 600, 350", "380, 600, 120", "492, 600, 8", "150, 250, 250", "380, 250, 120", "492, 250, 8" })
     public void testaRecebeAditivo(int quantidadeNoTanque, int quantidadeAditivo, int resultadoEsperado) {
         CentroDistribuicao centroDistribuicao = new CentroDistribuicao(quantidadeNoTanque, 0, 0, 0);
         int resultado = centroDistribuicao.recebeAditivo(quantidadeAditivo);
@@ -16,7 +15,7 @@ class AbasteceTeste {
     }
 
     @ParameterizedTest
-    @CsvSource({ "0, 0, -1", "5000, 0, -1", "10000, 0, -1", "0, -10, -1", "5000,-10, -1", "10000, -10, -1", "0, 7500, 7500", "5000, 7500, 5000", "10000, 7500, 0" })
+    @CsvSource({ "2000, 10000, 8000", "8000, 10000, 2000", "9850, 10000, 150", "2000, 10100, 8000", "8000, 10100, 2000", "9850, 10100, 150", "2000, 250, 250", "8000, 250, 250", "9850, 250, 150" })
     public void testaRecebeGasolina(int quantidadeNoTanque, int quantidadeGasolina, int resultadoEsperado) {
         CentroDistribuicao centroDistribuicao = new CentroDistribuicao(0, quantidadeNoTanque, 0, 0);
         int resultado = centroDistribuicao.recebeGasolina(quantidadeGasolina);
@@ -24,11 +23,10 @@ class AbasteceTeste {
     }
 
     @ParameterizedTest
-    @CsvSource({ "0, 0, 0, -1", "1000, 1000, 0, -1", "1250, 1250, 0, -1", "0, 0, -10, -1", "1000, 1000, -10, -1", "1250, 1250, -10, -1", "0, 0, 1500, 1500", "1000, 1000, 1500, 500", "1250, 1250, 1500, 0" })
+    @CsvSource({ "0, 0, 2500, 2500", "1000, 1000, 2500, 500", "1250, 1250, 2500, 0", "0, 0, 2600, 2500", "1000, 1000, 2600, 500", "1250, 1250, 2600, 0", "0, 0, 250, 250", "1000, 1000, 250, 250", "1250, 1250, 250, 0" })
     public void testaRecebeAlcool(int quantidadeNoTanque1, int quantidadeNoTanque2, int quantidadeAlcool, int resultadoEsperado) {
         CentroDistribuicao centroDistribuicao = new CentroDistribuicao(0, 0, quantidadeNoTanque1, quantidadeNoTanque2);
         int resultado = centroDistribuicao.recebeAlcool(quantidadeAlcool);
         Assert.assertEquals(resultadoEsperado, resultado);
     }
-
 }
